ff7cec8a8296c01ed4fa512bf3e47b08e9cb2c40
cari878 <riccardocarinelli05@gmail.com>
2026-07-02 16:43:45 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 // INCOLLA QUI I DATI DEL TUO PROGETTO SUPABASE
 // Li trovi in Supabase: Project Settings > API
 
-const SUPABASE_URL = "INCOLLA_QUI_SUPABASE_URL";
+const SUPABASE_URL = "https://esdequidhtzxhexefdyv.supabase.co";
 const SUPABASE_ANON_KEY = "sb_publishable_hL_I0hHOLWuyf28NTMPchQ_VIhV6fsM";
