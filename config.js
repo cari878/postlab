514f4a8838532b6eace644481a1a393a049700af
cari878 <riccardocarinelli05@gmail.com>
2026-07-02 16:29:05 +0200

    Update config.js

--- config.js
+++ config.js
@@ -2,4 +2,4 @@
 // Li trovi in Supabase: Project Settings > API
 
 const SUPABASE_URL = "INCOLLA_QUI_SUPABASE_URL";
-const SUPABASE_ANON_KEY = "INCOLLA_QUI_SUPABASE_ANON_KEY";
+const SUPABASE_ANON_KEY = "sb_publishable_hL_I0hHOLWuyf28NTMPchQ_VIhV6fsM";
